272436376618d568b2b284f3c201ec294585e83d
thakkarjinal <thakkarjinal29@gmail.com>
2020-07-24 21:30:06 +0530

    Survey App: use session instead of constant to save responses

--- flask/flask-survey/app.py
+++ flask/flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -8,12 +8,11 @@ app.debug = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-RESPONSES = []
-
 @app.route('/')
 def homepage():
     title = survey.title
     instruction = survey.instructions
+    session["responses"] = []
     return render_template("homepage.html", title=title, instruction=instruction)
 
 @app.route('/question/<int:id>')
@@ -23,10 +22,11 @@ def questions(id):
     to the next unanswered question.
     If all the questions are answered it takes to the 0th question.
     """
-    if id >= len(survey.questions) or len(RESPONSES) != id:
-        if len(RESPONSES) < len(survey.questions):
+    responses = session["responses"]
+    if id >= len(survey.questions) or len(responses) != id:
+        if len(responses) < len(survey.questions):
             flash("You can't enter random question id in the URL!") 
-            return redirect('/question/{}'.format(len(RESPONSES)))
+            return redirect('/question/{}'.format(len(responses)))
     question = survey.questions[id].question
     choices = survey.questions[id].choices
     return render_template("question.html", question=question, choices=choices)
@@ -37,14 +37,14 @@ def answer():
     Saving the answer and redirecting to the next appropriate question. Once all the questions are
     answered the responses are reset to be empty.    
     """
-    global RESPONSES
-    RESPONSES.append(request.form['choice'])
-    next_question_id = len(RESPONSES)
+    responses = session["responses"]
+    responses.append(request.form['choice'])
+    session["responses"] = responses
+    next_question_id = len(responses)
     total_questions = len(survey.questions)
-    if len(RESPONSES) < total_questions:
+    if len(responses) < total_questions:
         return redirect('/question/{}'.format(next_question_id))
     else:
-        responses = RESPONSES
-        RESPONSES = []
+        session["responses"] = []
         return render_template('thank_you.html', responses=responses, questions=survey.questions)
         
